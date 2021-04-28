f001cceb15922e592a2f60c73bcda09d791ea38f
AnnaWilhelmm <73894899+AnnaWilhelmm@users.noreply.github.com>
2021-04-28 14:00:38 +0200

    first try

--- frontend/src/app/pages/header/header.component.ts
+++ frontend/src/app/pages/header/header.component.ts
@@ -1,15 +1,26 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { MunicipalityService } from 'src/app/services/municipality.service';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss']
 })
-export class HeaderComponent implements OnInit {
 
-  constructor() { }
+export class HeaderComponent implements OnInit {
+  // Skapa interface för municipality istället för any[]
+  municipalities: any[];
+
+  constructor(private municipalityService: MunicipalityService, private router: Router) { }
 
   ngOnInit(): void {
+
+    this.municipalityService.getMunicipalities().subscribe((municipalities: any[]) => {
+      this.municipalities = municipalities;
+    })
   }
+
+
 
 }
