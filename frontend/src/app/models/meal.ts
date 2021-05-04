a107ec49f6605e322e35b1d58422d86ebae4860d
AnnaWilhelmm <73894899+AnnaWilhelmm@users.noreply.github.com>
2021-05-04 15:05:15 +0200

    icons

--- frontend/src/app/models/meal.ts
+++ frontend/src/app/models/meal.ts
@@ -3,4 +3,35 @@ export class Meal {
   mealName: string;
   mealDate: Date;
   foodSpecs: string[];
+
+  // foodSpecsHTML = function() : string {
+  //   let returnString : string = '';
+  //   this.foodSpecs.forEach(foodSpec => {
+  //     if(foodSpec === 'vego') {
+  //       returnString += `<i class="fas fa-seedling"></i>`;
+  //     } else if(foodSpec === 'gurka') {
+  //       returnString += `<i class="fas fa-pepper-hot"></i>`;
+  //     } else if(foodSpec === 'pig') {
+  //       returnString += `<i class="fas fa-bacon"></i>`;
+  //     }
+  //   });
+  //   return returnString;
+  // }
+
+  constructor(){}
+
+  foodSpecsHTML() : string{
+    let returnString : string = '';
+    this.foodSpecs.forEach(foodSpec => {
+      if(foodSpec === 'vego') {
+        returnString += `<i class="fas fa-seedling"></i>`;
+      } else if(foodSpec === 'gurka') {
+        returnString += `<i class="fas fa-pepper-hot"></i>`;
+      } else if(foodSpec === 'pig') {
+        returnString += `<i class="fas fa-bacon"></i>`;
+      }
+    });
+    return returnString;
+  }
+
 }
