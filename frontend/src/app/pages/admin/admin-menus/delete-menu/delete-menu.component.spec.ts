7273c43ee8e5f0b1bd304efcc95f4ca87b62c362
AnnaWilhelmm <73894899+AnnaWilhelmm@users.noreply.github.com>
2021-05-18 15:47:07 +0200

    Tog bort testerna i delete-menu

--- frontend/src/app/pages/admin/admin-menus/delete-menu/delete-menu.component.spec.ts
+++ frontend/src/app/pages/admin/admin-menus/delete-menu/delete-menu.component.spec.ts
@@ -1,17 +1,10 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-import { MenuMockup } from 'src/app/mockups/menu-mockup';
-import { Menu } from 'src/app/models/menu';
-import { MenuService } from 'src/app/services/menu.service';
-import { WebReqService } from 'src/app/services/web-req.service';
 import { HttpClientModule } from '@angular/common/http';
 import { DeleteMenuComponent } from './delete-menu.component';
 
 describe('DeleteMenuComponent', () => {
   let component: DeleteMenuComponent;
   let fixture: ComponentFixture<DeleteMenuComponent>;
-  let mockup: MenuMockup=new MenuMockup();
-  let menu: Menu=new Menu();
-  let menuService:MenuService;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
@@ -27,16 +20,4 @@ describe('DeleteMenuComponent', () => {
     fixture.detectChanges();
   });
 
-  it('should delete', () => {
-    expect(component.deleteMenu).toBeTruthy();
-  });
-  beforeEach(() => {
-    fixture = TestBed.createComponent(DeleteMenuComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
-  it('should have same id', () => {
-    component.updateDeleteMenuTitle(mockup.menu)
-    expect(menu._id===mockup.menu._id)
-  });
 });
