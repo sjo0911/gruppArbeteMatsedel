f27a0d6ccb03f8cb018a3198b316dfbba71cf2d5
AnnaWilhelmm <73894899+AnnaWilhelmm@users.noreply.github.com>
2021-05-19 14:39:20 +0200

    Valideringar matsedel

--- frontend/src/app/pages/admin/admin-menus/create-menu/create-menu.component.ts
+++ frontend/src/app/pages/admin/admin-menus/create-menu/create-menu.component.ts
@@ -3,6 +3,7 @@ import { MenuService } from './../../../../services/menu.service';
 import { Component, OnInit } from '@angular/core';
 import  Swal  from 'sweetalert2';
 import { Alert } from 'src/assets/alert';
+import { DatePipe } from '@angular/common';
 
 @Component({
   selector: 'app-create-menu',
@@ -17,10 +18,15 @@ export class CreateMenuComponent implements OnInit {
   }
 
   createMenu(menuName : string, startDate : Date, endDate : Date) {
+    let datePipe: DatePipe = new DatePipe('en-US');
     if(menuName.length < 1) {
       this.alert.showAlert('', 'Input för menynamnet är för kort. Testa igen!', 'warning');
     } else if (menuName.length > 40) {
       this.alert.showAlert('', 'Input för menynamnet är för långt. Testa igen!', 'warning');
+    } else if (!(datePipe.transform(startDate, 'EEEE') === 'Monday')) {
+      this.alert.showAlert('', 'Startdatum för matsedeln måste vara en måndag!', 'warning');
+    } else if (!(datePipe.transform(endDate, 'EEEE') === 'Sunday')) {
+      this.alert.showAlert('', 'Slutdatum för matsedeln måste vara en söndag!', 'warning');
     } else {
       let menu : Menu = new Menu();
       menu.menuName = menuName;
@@ -28,6 +34,7 @@ export class CreateMenuComponent implements OnInit {
       menu.endDate = endDate;
       this.menuService.postMenu(menu).subscribe(() => {
       })
+      this.alert.showAlertAndUpdatePage('Sparad!', 'Matsedeln har blivit sparad.', 'success');
     }
   }
 }
--- frontend/src/app/pages/admin/admin-menus/update-menu/update-menu.component.ts
+++ frontend/src/app/pages/admin/admin-menus/update-menu/update-menu.component.ts
@@ -2,6 +2,8 @@ import { MenuService } from './../../../../services/menu.service';
 import { Observable } from 'rxjs';
 import { Menu } from './../../../../models/menu';
 import { Component, OnInit, Input } from '@angular/core';
+import { Alert } from 'src/assets/alert';
+import { DatePipe } from '@angular/common';
 
 @Component({
   selector: 'app-update-menu',
@@ -17,7 +19,7 @@ export class UpdateMenuComponent implements OnInit {
   chosenMenuStartDate : Date;
   chosenMenuEndDate : Date;
 
-  constructor(private menuService: MenuService) {
+  constructor(private menuService: MenuService, private alert : Alert) {
     this.editMenuTitle = "Välj matsedel att redigera: "
     this.menuNameToEdit = "";
 
@@ -36,11 +38,25 @@ export class UpdateMenuComponent implements OnInit {
   }
 
   saveEditedMenu(menuName : string, startDate : Date, endDate : Date) {
-    this.menuToEdit.menuName = menuName;
-    this.menuToEdit.startDate = startDate;
-    this.menuToEdit.endDate = endDate;
-    this.menuService.updateMenu(this.menuToEdit).subscribe(() => {
-    })
+    let datePipe: DatePipe = new DatePipe('en-US');
+    if (menuName === '') {
+      this.alert.showAlert('', 'Du måste välja en matsedel att uppdatera.', 'warning');
+    } else if (menuName.length < 1) {
+      this.alert.showAlert('', 'Input för menynamnet är för kort. Testa igen!', 'warning');
+    } else if (menuName.length > 40) {
+      this.alert.showAlert('', 'Input för menynamnet är för långt. Testa igen!', 'warning');
+    } else if (!(datePipe.transform(startDate, 'EEEE') === 'Monday')) {
+      this.alert.showAlert('', 'Startdatum för matsedeln måste vara en måndag!', 'warning');
+    } else if (!(datePipe.transform(endDate, 'EEEE') === 'Sunday')) {
+      this.alert.showAlert('', 'Slutdatum för matsedeln måste vara en söndag!', 'warning');
+    } else {
+      this.menuToEdit.menuName = menuName;
+      this.menuToEdit.startDate = startDate;
+      this.menuToEdit.endDate = endDate;
+      this.menuService.updateMenu(this.menuToEdit).subscribe(() => {
+      })
+      this.alert.showAlertAndUpdatePage('Uppdaterad!', 'Matsedeln har blivit uppdaterad.', 'success');
+    }
   }
 
 
