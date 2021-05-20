4bf393312e6d6d4666362ee3af6ea97a21d70d2d
MarkusAltin <marcusaltin@hotmail.com>
2021-05-20 11:11:26 +0200

    NgOnInit för Admin-menus & Admin-schools

--- frontend/src/app/pages/admin/admin-schools/admin-schools.component.spec.ts
+++ frontend/src/app/pages/admin/admin-schools/admin-schools.component.spec.ts
@@ -2,13 +2,14 @@ import { Observable, of } from 'rxjs';
 import { MenuService } from './../../../services/menu.service';
 import { MunicipalityService } from './../../../services/municipality.service';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { AdminSchoolsComponent } from './admin-schools.component';
 import { Municipality } from 'src/app/models/municipality';
 
 describe('AdminSchoolsComponent', () => {
   let component: AdminSchoolsComponent;
   let fixture: ComponentFixture<AdminSchoolsComponent>;
+  let mockMenuService : MenuService;
+  let mockMunicipalityService : MunicipalityService;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
@@ -25,11 +26,27 @@ describe('AdminSchoolsComponent', () => {
     fixture = TestBed.createComponent(AdminSchoolsComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
+    mockMenuService = TestBed.inject(MenuService);
+    mockMunicipalityService = TestBed.inject(MunicipalityService);
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should call NgOnInit() & getMunicipalities()', () => {
+    component.ngOnInit();
+    expect(component.municipalities).toBeDefined();
+    expect(component.municipalities).not.toBeNull();
+    expect(component.municipalities).toEqual([]);
+  })
+
+  it('should call NgOnInit() & getMenus()', () => {
+    component.ngOnInit();
+    expect(component.menus).toBeDefined();
+    expect(component.menus).not.toBeNull();
+    expect(component.menus).toEqual([]);
+  })
 });
 
 class MunicipalityServiceStub {
