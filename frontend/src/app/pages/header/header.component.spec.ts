abc3ca2fa1d5a5e56c8436a636563cbb3e832a2f
AnnaWilhelmm <73894899+AnnaWilhelmm@users.noreply.github.com>
2021-05-24 16:01:42 +0200

    tester header och loginFailed

--- frontend/src/app/pages/header/header.component.spec.ts
+++ frontend/src/app/pages/header/header.component.spec.ts
@@ -8,12 +8,26 @@ import { Observable, of } from 'rxjs';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { HeaderComponent } from './header.component';
+import { DOMHelper } from 'src/app/mockups/DOM-helper';
+import { By } from '@angular/platform-browser';
 
 describe('HeaderComponent', () => {
   let component: HeaderComponent;
   let fixture: ComponentFixture<HeaderComponent>;
+  let dh: DOMHelper<HeaderComponent>;
+  let municipalityServiceMock: any;
+  let sharingServiceMock: any;
+  let dateHandlerServiceMock: any;
 
   beforeEach(async () => {
+    municipalityServiceMock = jasmine.createSpyObj('MunicipalityService', ['getMunicipalities']);
+    municipalityServiceMock.getMunicipalities.and.returnValue(of([]));
+
+    sharingServiceMock = jasmine.createSpyObj('SharingService', ['setMenu']);
+
+    dateHandlerServiceMock = jasmine.createSpyObj('DateHandlerService', ['getWeeks']);
+    dateHandlerServiceMock.getWeeks.and.returnValue(of([]));
+
     await TestBed.configureTestingModule({
       declarations: [ HeaderComponent ],
       imports: [
@@ -26,7 +40,7 @@ describe('HeaderComponent', () => {
       providers: [
         {provide: MenuService, useClass: MenuServiceStub},
         {provide: Alert, useClass: AlertStub}  ,
-        {provide: MunicipalityService, useClass: MunicipalityServiceStub}, ],
+        {provide: MunicipalityService, useValue: municipalityServiceMock}, ],
     })
     .compileComponents();
   });
@@ -35,11 +49,154 @@ describe('HeaderComponent', () => {
     fixture = TestBed.createComponent(HeaderComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
+    dh = new DOMHelper(fixture);
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should contain two buttons', () => {
+    expect(dh.countFromTagName('button')).toEqual(2);
+  });
+
+  it('should call getMunicipalities() from municipalityService on NgOnInit()', () => {
+    // spyOn(municipalityServiceMock, 'getMunicipalities');
+    // component.ngOnInit();
+    expect(component.municipalities).toBeDefined();
+    expect(component.municipalities).not.toBeNull();
+    expect(component.municipalities).toEqual([]);
+
+    expect(municipalityServiceMock.getMunicipalities).toHaveBeenCalledTimes(1);
+  });
+
+  it('should call method chooseMunicipality() when a municipality is clicked in dropdown', (done) => {
+    spyOn(component, 'chooseMunicipality');
+    component.municipalities = [
+      {_id:'abc', municipalityName: 'Skellefteå', schools: [{_id:'abc123', schoolName:'Balderskolan', _menuId:'123'}]},
+      {_id:'abc', municipalityName: 'Umeå', schools: [{_id:'cde456', schoolName:'Dragonskolan', _menuId:'123'}]}];
+       fixture.detectChanges();
+       fixture.whenStable().then(() => {
+        const dropDown = fixture.debugElement.queryAll(By.css('div.navbar-dropdown'))[0];
+        dropDown.children[0].nativeElement.click();
+        fixture.detectChanges();
+        fixture.whenStable().then(() => {
+          expect(component.chooseMunicipality).toHaveBeenCalledTimes(1);
+         });
+       });
+       done();
+  });
+
+  it('should call method chooseMunicipality() with chosen(first) municipality', (done) => {
+    spyOn(component, 'chooseMunicipality');
+    component.municipalities = [
+      {_id:'abc', municipalityName: 'Skellefteå', schools: [{_id:'abc123', schoolName:'Balderskolan', _menuId:'123'}]},
+      {_id:'abc', municipalityName: 'Umeå', schools: [{_id:'cde456', schoolName:'Dragonskolan', _menuId:'123'}]}];
+       fixture.detectChanges();
+       fixture.whenStable().then(() => {
+        const dropDownMunicipalities = fixture.debugElement.queryAll(By.css('div.navbar-dropdown'))[0];
+        expect(dropDownMunicipalities.children.length).toBe(2);
+        dropDownMunicipalities.children[0].nativeElement.click();
+        fixture.detectChanges();
+        fixture.whenStable().then(() => {
+          expect(component.chooseMunicipality).toHaveBeenCalledWith(component.municipalities[0]);
+         });
+       });
+       done();
+  });
+
+  it('should call method chooseSchool() with chosen school when a school is clicked in dropdown', (done) => {
+    spyOn(component, 'chooseSchool');
+    component.municipalities = [
+      {_id:'abc', municipalityName: 'Skellefteå', schools: [{_id:'abc123', schoolName:'Balderskolan', _menuId:'123'}]},
+      {_id:'abc', municipalityName: 'Umeå', schools: [{_id:'cde456', schoolName:'Dragonskolan', _menuId:'123'}]}];
+       fixture.detectChanges();
+       fixture.whenStable().then(() => {
+        const dropDownMunicipalities = fixture.debugElement.queryAll(By.css('div.navbar-dropdown'))[0];
+        dropDownMunicipalities.children[0].nativeElement.click();
+        expect(dropDownMunicipalities.children.length).toBe(2);
+        fixture.detectChanges();
+        fixture.whenStable().then(() => {
+          const dropDownSchools = fixture.debugElement.queryAll(By.css('div.navbar-dropdown'))[1];
+          expect(dropDownSchools.children.length).toBe(1);
+          dropDownSchools.children[0].nativeElement.click();
+          fixture.detectChanges();
+          fixture.whenStable().then(() => {
+            expect(component.chooseSchool).toHaveBeenCalledTimes(1);
+            expect(component.chooseSchool).toHaveBeenCalledWith(component.municipalities[0].schools[0]);
+          });
+         });
+       });
+       done();
+  });
+
+  it('should call method chooseWeek() with chosen week when a week is clicked in dropdown', (done) => {
+    spyOn(component, 'chooseWeek');
+    component.municipalities = [
+      {_id:'abc', municipalityName: 'Skellefteå', schools: [{_id:'abc123', schoolName:'Balderskolan', _menuId:'123'}]},
+      {_id:'abc', municipalityName: 'Umeå', schools: [{_id:'cde456', schoolName:'Dragonskolan', _menuId:'123'}]}];
+    component.weeks = [{startDate: new Date('2021-05-24'), endDate: new Date('2021-05-30'), weekNr: '21', days: []}];
+       fixture.detectChanges();
+       fixture.whenStable().then(() => {
+        const dropDownMunicipalities = fixture.debugElement.queryAll(By.css('div.navbar-dropdown'))[0];
+        dropDownMunicipalities.children[0].nativeElement.click();
+        expect(dropDownMunicipalities.children.length).toBe(2);
+        fixture.detectChanges();
+        fixture.whenStable().then(() => {
+          const dropDownSchools = fixture.debugElement.queryAll(By.css('div.navbar-dropdown'))[1];
+          expect(dropDownSchools.children.length).toBe(1);
+          dropDownSchools.children[0].nativeElement.click();
+          fixture.detectChanges();
+          fixture.whenStable().then(() => {
+            const dropDownWeeks = fixture.debugElement.queryAll(By.css('div.navbar-dropdown'))[2];
+            dropDownWeeks.children[0].nativeElement.click();
+            fixture.detectChanges();
+            fixture.whenStable().then(() => {
+              expect(component.chooseWeek).toHaveBeenCalledTimes(1);
+              expect(component.chooseWeek).toHaveBeenCalledWith(component.weeks[0]);
+            });
+          });
+         });
+       });
+       done();
+  });
+
+  // it('should call dateHandlerService.getWeeks() in chooseSchool() when school is chosen in dropdown', (done) => {
+
+  //   component.municipalities = [
+  //     {_id:'abc', municipalityName: 'Skellefteå', schools: [{_id:'abc123', schoolName:'Balderskolan', _menuId:'123'}]}];
+  //      fixture.detectChanges();
+  //      fixture.whenStable().then(() => {
+  //       const dropDownMunicipalities = fixture.debugElement.queryAll(By.css('div.navbar-dropdown'))[0];
+  //       dropDownMunicipalities.children[0].nativeElement.click();
+  //       fixture.detectChanges();
+  //       fixture.whenStable().then(() => {
+  //         const dropDownSchools = fixture.debugElement.queryAll(By.css('div.navbar-dropdown'))[1];
+  //         dropDownSchools.children[0].nativeElement.click();
+  //         fixture.detectChanges();
+  //         fixture.whenStable().then(() => {
+  //           expect(dateHandlerServiceMock.getWeeks).toHaveBeenCalledTimes(1);
+  //         });
+  //        });
+  //      });
+  //      done();
+  // });
+
+  // it('should call sharingService.setMenu() in chooseSchool() when school is chosen in dropdown', () => {
+  // });
+
+  it('should call previousWeekClick() when "paginatorLeft" button is clicked', () => {
+    spyOn(component, 'previousWeekClick');
+    dh.clickButton('arrow_back_ios');
+    expect(component.previousWeekClick).toHaveBeenCalledTimes(1);
+  });
+
+  it('should call nextWeekClick() when "paginatorRight" button is clicked', () => {
+    spyOn(component, 'nextWeekClick');
+    dh.clickButton('arrow_forward_ios');
+    expect(component.nextWeekClick).toHaveBeenCalledTimes(1);
+  });
+
 });
 
 
--- frontend/src/app/pages/login-failed/login-failed.component.spec.ts
+++ frontend/src/app/pages/login-failed/login-failed.component.spec.ts
@@ -1,13 +1,28 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, fakeAsync, TestBed } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
+import { RouterTestingModule } from '@angular/router/testing';
+import { Location } from '@angular/common';
 
 import { LoginFailedComponent } from './login-failed.component';
+import { Router } from '@angular/router';
+import { exception } from 'node:console';
+import { DOMHelper } from 'src/app/mockups/DOM-helper';
+
+class TempRouter{}
 
 describe('LoginFailedComponent', () => {
   let component: LoginFailedComponent;
   let fixture: ComponentFixture<LoginFailedComponent>;
+  let dh: DOMHelper<LoginFailedComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
+      imports:[RouterTestingModule.withRoutes(
+        [
+          { path:'loginFailed', component: TempRouter},
+          { path: '', component: TempRouter}
+        ]
+      )],
       declarations: [ LoginFailedComponent ]
     })
     .compileComponents();
@@ -17,14 +32,54 @@ describe('LoginFailedComponent', () => {
     fixture = TestBed.createComponent(LoginFailedComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
+    dh = new DOMHelper(fixture);
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
 
-  // it('should contain ', () => {
-  //   expect(component).toBeTruthy();
+  it('should contain a label with text "Inloggning misslyckades!"', () => {
+    expect(dh.singleTextFromTagName("p").trim()).toBe("Inloggning misslyckades!");
+  });
+
+  it('should contain only one button', () => {
+    expect(dh.countFromTagName("button")).toEqual(1);
+  });
+
+  it('should contain button with text "Till startsidan"', () => {
+    expect(dh.singleTextFromTagName("button").trim()).toBe("Till startsidan");
+  });
+
+  it('should be in route / (/loginFailed) before first button click', () => {
+    const location = TestBed.inject(Location);
+    expect(location.path()).toBe('');
+
+    // const button = fixture.debugElement.query(By.css('button'));
+    // if(expect(button.nativeNode.outerText).toBe("Till startsidan")){
+    //   expect(location.path()).toBe('loginFailed');
+    // }
+
+  });
+
+  // it('should navigate to / on "Till startsidan" button click', () => {
+  //   // const router = TestBed.inject(Router);
+  //   // spyOn(router, 'navigateByUrl');
+  //   // dh.clickButton('Till startsidan');
+  //   // expect(router.navigateByUrl).toHaveBeenCalledWith(router.createUrlTree(['']), {skipLocationChange: false, replaceUrl: false, state: undefined});
+
+  //   // done();
+
+  //   // const location = TestBed.inject(Location);
+  //   // const button = fixture.debugElement.queryAll(By.css('button'));
+  //   // if(expect(button[0].nativeNode.outerText).toBe("Till startsidan")){
+  //   //   const nativeButton: HTMLButtonElement=button[0].nativeElement;
+  //   //   nativeButton.click();
+  //   //   fixture.detectChanges();
+  //   //   fixture.whenStable().then(()=>{
+  //   //   expect(location.path()).toBe('')
+  //   // })
+  //   // }
   // });
 
 });
