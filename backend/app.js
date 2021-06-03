ff46fa61ceb6436138c74e9fef51f53b50bffdd8
Simon <jonasson_simon@hotmail.com>
2021-06-03 12:35:19 +0200

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -24,7 +24,7 @@ app.get('/', function (req, res) {
 
 app.listen(process.env.PORT || 8080);
 
-
+//
 app.use(function (req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Credentials", "true")
