7de3fa3b8690413ca79844dac48eadc6716e6ecc
Simon <jonasson_simon@hotmail.com>
2021-06-07 10:30:19 +0200

    fixed cors issue on localhost

--- backend/app.js
+++ backend/app.js
@@ -11,8 +11,13 @@ app.use(express.json()) // To parse the incoming requests with JSON payloads
 app.use(express.static('./dist/'));
 app.use(function(req, res, next) {
     // The 'x-forwarded-proto' check is for Heroku
-    if (!req.secure && req.get('x-forwarded-proto') !== 'https') {
-        return res.redirect('https://' + req.get('host') + req.url);
+    console.log(req.get('host'))
+    if(req.get('host') != "localhost:8080" && req.get('host') != "http://localhost:4200/"){
+        console.log("hej");
+        if (!req.secure && req.get('x-forwarded-proto') !== 'https') {
+            return res.redirect('https://' + req.get('host') + req.url);
+        }
+        next();
     }
     next();
 })
