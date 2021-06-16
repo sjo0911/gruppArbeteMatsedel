ad2c64a4a3f62c718ff026f44ccdfd176617d743
AnnaWilhelmm <73894899+AnnaWilhelmm@users.noreply.github.com>
2021-06-16 11:55:34 +0200

    tester updateUser

--- frontend/src/app/pages/admin/admin-user/update-user/update-user.component.spec.ts
+++ frontend/src/app/pages/admin/admin-user/update-user/update-user.component.spec.ts
@@ -6,12 +6,23 @@ import { Observable, of } from 'rxjs';
 import { UserService } from 'src/app/services/user.service';
 import { Alert } from 'src/assets/alert';
 import { MunicipalityService } from 'src/app/services/municipality.service';
+import { DOMHelper } from '../../../../mockups/DOM-helper';
+import { By } from '@angular/platform-browser';
 
 describe('UpdateUserComponent', () => {
   let component: UpdateUserComponent;
   let fixture: ComponentFixture<UpdateUserComponent>;
+  let dh: DOMHelper<UpdateUserComponent>;
+  let municipalityServiceMock: any;
+  let userServiceMock: any;
 
   beforeEach(async () => {
+    municipalityServiceMock = jasmine.createSpyObj('MunicipalityService', ['getSchools']);
+    municipalityServiceMock.getSchools.and.returnValue(of([]));
+
+    userServiceMock = jasmine.createSpyObj('UserService', ['getUsers']);
+    userServiceMock.getUsers.and.returnValue(of([]));
+
     await TestBed.configureTestingModule({
       declarations: [ UpdateUserComponent ],
       providers: [
@@ -19,6 +30,8 @@ describe('UpdateUserComponent', () => {
         {provide : UserService, useClass: UserServiceStub},
         {provide: Alert, useClass: AlertStub},
         {provide: MunicipalityService, useClass: MunicipalityServiceStub},
+        {provide: MunicipalityService, useValue: municipalityServiceMock},
+        {provide: UserService, useValue: userServiceMock}
       ]
     })
     .compileComponents();
@@ -28,6 +41,7 @@ describe('UpdateUserComponent', () => {
     fixture = TestBed.createComponent(UpdateUserComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
+    dh = new DOMHelper(fixture);
   });
 
   describe('Create', () => {
@@ -35,6 +49,71 @@ describe('UpdateUserComponent', () => {
       expect(component).toBeTruthy();
     });
   });
+
+  describe('HTML', () => {
+    it('should contain 4 inputs', () => {
+      expect(dh.countFromTagName("input.input-field")).toEqual(4);
+    });
+
+    it('should contain button with text "Uppdatera användare"', () => {
+      expect(dh.singleTextFromTagName("button")).toBe('Uppdatera användare');
+    });
+  });
+
+  describe('Check methods when buttons are clicked', () => {
+    it('should call updateUser() when "create-button" button is clicked', () => {
+      spyOn(component, 'updateUser');
+      dh.clickButton('Uppdatera användare');
+      expect(component.updateUser).toHaveBeenCalledTimes(1);
+    });
+
+    it('should call clickAdmin() when "permission-checkbox" checkbox is clicked', () => {
+      spyOn(component, 'clickAdmin');
+      let input = fixture.debugElement.query(By.css('#admin')).nativeElement;
+      expect(input.checked).toBeFalsy();
+      input.click();
+      fixture.detectChanges();
+      expect(input.checked).toBeTruthy();
+      expect(component.clickAdmin).toHaveBeenCalledTimes(1);
+    });
+
+  });
+
+  describe('Check methods when dropdown is clicked', () => {
+    // it('should call method chooseUserToUpdate() when a user is clicked in dropdown', (done) => {
+    //   spyOn(component, 'chooseUserToUpdate');
+    //   component.users = of([
+    //     {_id:'abc', firstName: 'Leif', lastName: 'Olsson', schoolIds: [], permissions: [], menuIds: [], password: 'hejhej'},
+    //     {_id:'abc123', firstName: 'Gunilla', lastName: 'Persson', schoolIds: [], permissions: [], menuIds: [], password: 'hejhej'}]);
+    //      fixture.detectChanges();
+    //      fixture.whenStable().then(() => {
+    //       const dropDown = fixture.debugElement.queryAll(By.css('a'))[1];
+    //       dropDown.nativeElement.click();
+    //       // dropDown.nativeNode.options[1].click();
+    //       // dropDown.nativeNode.dispatchEvent(new Event('change'));
+    //       fixture.detectChanges();
+    //       fixture.whenStable().then(() => {
+    //         expect(component.chooseUserToUpdate).toHaveBeenCalledTimes(1);
+    //        });
+    //      });
+    //      done();
+    // });
+  });
+
+  describe('Check ngOnInit', () => {
+    it('should call getSchools() from municipalityService on NgOnInit()', () => {
+      expect(component.schoolsToChoose).toBeDefined();
+      expect(component.schoolsToChoose).not.toBeNull();
+      expect(municipalityServiceMock.getSchools).toHaveBeenCalledTimes(1);
+    });
+
+    it('should call getUsers() from userService on NgOnInit()', () => {
+      expect(component.users).toBeDefined();
+      expect(component.users).not.toBeNull();
+      expect(userServiceMock.getUsers).toHaveBeenCalledTimes(1);
+    });
+  });
+
 });
 
 class AuthServiceStub{
